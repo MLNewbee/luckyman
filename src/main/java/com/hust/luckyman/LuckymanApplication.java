4cfc8bd1e665dc98a72712798cee73310be1133b
MLNewbee <574052658@qq.com>
2019-04-15 11:04:50 +0800

    Spring boot initial files

---
+++ src/main/java/com/hust/luckyman/LuckymanApplication.java
@@ -0,0 +1,13 @@
+package com.hust.luckyman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckymanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckymanApplication.class, args);
+    }
+
+}
